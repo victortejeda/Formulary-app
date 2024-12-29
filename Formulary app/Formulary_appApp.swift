be60ccd03ce0c67534a43bf88c64e48cc41baa3c
victortejeda <victorfernandotejeda@gmail.com>
2024-12-29 02:41:26 -0400

    Initial Commit

---
+++ Formulary app/Formulary_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Formulary_appApp.swift
+//  Formulary app
+//
+//  Created by Victor Tejeda on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Formulary_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
